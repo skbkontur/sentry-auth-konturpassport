c5dfc80278d42a82b47c5095ba6b7d6c385232ac
mlefebvre <maxence.lefebvre@soprasteria.com>
2018-01-25 15:12:53 +0100

    fix(constants): forgot a part of the function

--- sentry_auth_openid/constants.py
+++ sentry_auth_openid/constants.py
@@ -1,20 +1,10 @@
 from __future__ import absolute_import, print_function
-
 from django.conf import settings
-
-
 AUTHORIZE_URL = getattr(settings, 'OPENID_AUTHORIZE_URL', None)
-
 ACCESS_TOKEN_URL = getattr(settings, 'OPENID_TOKEN_URL', None)
-
 CLIENT_ID = getattr(settings, 'OPENID_CLIENT_ID', None)
-
 CLIENT_SECRET = getattr(settings, 'OPENID_CLIENT_SECRET', None)
-
-ERR_INVALID_RESPONSE = 'Unable to fetch user information.  Please check the log.'
-
 SCOPE = getattr(settings, 'OPENID_SCOPE', 'openid email')
-
-DATA_VERSION = '1'
-
 EMAIL_PROP = getattr(settings, 'OPENID_EMAIL_PROP', 'email')
+ERR_INVALID_RESPONSE = 'Unable to fetch user information.  Please check the log.'
+DATA_VERSION = '1'
