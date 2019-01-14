22099a6f1bee7c73731f11c280856e53beb3b4a3
Alexander <DXD42@users.noreply.github.com>
2019-01-14 18:22:26 +0500

    provider renamed to KonturPassport

--- sentry_auth_openid/__init__.py
+++ sentry_auth_openid/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from sentry.auth import register
 
-from .provider import OpenIDOAuth2Provider
+from .provider import KonturPassport2Provider
 
-register('openid', OpenIDOAuth2Provider)
+register('openid', KonturPassport2Provider)
